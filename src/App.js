0ea6b98bf36b60ea3d4d957b342fdebfcad89bc6
yeeun <yenn2322@gmail.com>
2020-02-23 00:22:56 +0900

    #2.4 Protection with PropTypes

--- src/App.js
+++ src/App.js
@@ -1,27 +1,36 @@
 import React from "react";
-
+import PropTypes from "prop-types";
 //props.Fav = {fav}
-function Food({ name }) {
+function Food({ name, rating }) {
   return (
     <div>
       <h3>I like {name}</h3>
+      <h4>{rating}</h4>
       {/* <img src={picture}></img> */}
     </div>
   );
 }
 
+Food.propTypes = {
+  name: PropTypes.string,
+  rating: PropTypes.number.isRequired
+};
+
 const FoodIlike = [
   {
     name: "kimchi",
-    key: 1
+    key: 1,
+    rating: 5
   },
   {
     name: "hamburger",
-    key: 2
+    key: 2,
+    rating: 4
   },
   {
     name: "pizza",
-    key: 3
+    key: 3,
+    rating: 3.3
   }
 ];
 
@@ -30,7 +39,11 @@ function App() {
     <div>
       {FoodIlike.map(current => console.log(current))}
       {FoodIlike.map(current => (
-        <Food name={current.name} key={current.key}></Food>
+        <Food
+          name={current.name}
+          key={current.key}
+          rating={current.rating}
+        ></Food>
       ))}
       {/* FoodIlike의 name과 image 각각 props로 넘겨줘도 되지만 object로 하나의 props로 넘겨서 사용하는것도 가능! */}
       {/* {FoodIlike.map(current => (
