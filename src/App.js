cdaecdb25a5c97c923b965e573c9855ac72849e6
yeeun <yenn2322@gmail.com>
2020-02-23 22:02:41 +0900

    #4.0 Fetching Movies from API

--- src/App.js
+++ src/App.js
@@ -1,40 +1,23 @@
 import React from "react";
+import axios from "axios";
 
 class App extends React.Component {
   state = {
-    count: 0,
-    isLoading: true
+    isLoading: true,
+    movies: []
   };
 
-  constructor(props) {
-    super(props);
-  }
+  getMovies = async () => {
+    const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+  };
 
   componentDidMount() {
-    console.log("Mount");
-
-    setInterval(() => {
-      this.setState(current => ({ isLoading: false }));
-    }, 6000);
+    this.getMovies();
   }
-  componentDidUpdate() {
-    console.log("Update");
-  }
-
-  add = () => {
-    this.setState(current => ({ count: current.count + 1 }));
-  };
-  minus = () => {
-    this.setState(current => ({ count: current.count - 1 }));
-  };
 
   render() {
-    console.log("render");
     return (
       <div>
-        <h1>The number is {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
         <h1>{this.state.isLoading ? "Loading..." : "Ready"}</h1>
       </div>
     );
