9c82005db839233f972d216fa18de6964c5b8f49
yeeun <yenn2322@gmail.com>
2020-02-24 21:38:46 +0900

    #4.2 Styling the Movies

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import axios from "axios";
 import Movie from "./Movie";
+import "./App.css";
 
 class App extends React.Component {
   state = {
@@ -27,9 +28,10 @@ class App extends React.Component {
   }
 
   render() {
+    const { movies } = this.state;
     return (
-      <div>
-        {this.state.movies.map(current => {
+      <div class="movies">
+        {movies.map(current => {
           console.log(current);
           return (
             <Movie
