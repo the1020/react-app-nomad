fd0ac6825f567670942f50e51e75ad8f7a669a30
yeeun <yenn2322@gmail.com>
2020-02-24 23:03:59 +0900

    #4.5 Cutting the summary

--- src/Movie.js
+++ src/Movie.js
@@ -4,7 +4,7 @@ import "./Movie.css";
 
 function Movie({ year, title, summary, poster, genres }) {
   return (
-    <div classNme="movie">
+    <div className="movie">
       <img src={poster} alt={title} title={title}></img>
       <div className="movie__data">
         <h3 className="movie__title">{title}</h3>
@@ -16,7 +16,7 @@ function Movie({ year, title, summary, poster, genres }) {
             </li>
           ))}
         </ul>
-        <p className="movie__summary">{summary}</p>
+        <p className="movie__summary">{summary.slice(0, 140)}...</p>
       </div>
     </div>
   );
