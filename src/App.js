59551a5c0ab425082b66c84fc94f92ccaa7b16ca
yeeun <yenn2322@gmail.com>
2020-02-26 22:53:52 +0900

    #6.4 Redirecting

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
       <Navigation></Navigation>
       <Route path="/" exact={true} component={Home}></Route>
       <Route path="/about" component={About}></Route>
-      <Route path="/movie-detail" component={Detail}></Route>
+      <Route path="/movie/:id" component={Detail}></Route>
     </HashRouter>
   );
 }
