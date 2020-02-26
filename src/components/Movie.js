b55850a92c9d37f5b3f3ce3c16abf391f9844e10
yeeun <yenn2322@gmail.com>
2020-02-26 22:20:42 +0900

    #6.3 Sharing Props Between Routes

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,24 +1,38 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 import "./Movie.css";
 
 function Movie({ year, title, summary, poster, genres }) {
   return (
-    <div className="movie">
-      <img src={poster} alt={title} title={title}></img>
-      <div className="movie__data">
-        <h3 className="movie__title">{title}</h3>
-        <h5 className="movie__year">{year}</h5>
-        <ul className="movie__genres">
-          {genres.map((current, index) => (
-            <li className="genres__genre" key={index}>
-              {current}
-            </li>
-          ))}
-        </ul>
-        <p className="movie__summary">{summary.slice(0, 140)}...</p>
+    <Link
+      to={{
+        pathname: "/movie-detail",
+        state: {
+          year,
+          title,
+          summary,
+          poster,
+          genres
+        }
+      }}
+    >
+      <div className="movie">
+        <img src={poster} alt={title} title={title}></img>
+        <div className="movie__data">
+          <h3 className="movie__title">{title}</h3>
+          <h5 className="movie__year">{year}</h5>
+          <ul className="movie__genres">
+            {genres.map((current, index) => (
+              <li className="genres__genre" key={index}>
+                {current}
+              </li>
+            ))}
+          </ul>
+          <p className="movie__summary">{summary.slice(0, 140)}...</p>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 }
 
