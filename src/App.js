79a3c459e7a3d6c5afefe1548e2b20d4b2f56c9d
yeeun <yenn2322@gmail.com>
2020-02-26 21:49:07 +0900

    #6.2 Building the Navigation

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from "react";
-import { HashRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom"; //BrowserRouter를 사용해도 되지만 github pages에 업로드 시 복잡...
 import About from "./routes/About";
 import Home from "./routes/Home";
+import Navigation from "./components/Navigation";
 
 function App() {
   return (
-    <HashRouter>
+    <BrowserRouter>
+      <Navigation></Navigation>
       <Route path="/" exact={true} component={Home}></Route>
       <Route path="/about" component={About}></Route>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
